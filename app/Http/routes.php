8e28eec54679f5e905a687b8dcd227aef280f53a
Zac Collins <zcollins@sscorp.com>
2016-05-13 17:44:41 -0400

    updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,4 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
+Route::resource('flyers', 'FlyersController');
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
 	<h1>Project Flyer</h1>
+
+	<a href="/flyers/create" class="btn btn-primary">Create a flyer</a>
+
 @stop
